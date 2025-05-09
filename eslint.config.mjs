e7cd0aadd1267707438e51c08cfb3817191024aa
Suyog <suyogsitaula090@gmail.com>
2025-05-09 09:55:09 +0545

    esling configuration changed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Custom overrides
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "no-unused-vars": "off",
+      "react-hooks/rules-of-hooks": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
